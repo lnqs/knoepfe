0136775e23536e95d49efc5a21806f83a490c45f
Simon Hayessen <simon@lnqs.io>
2022-03-05 01:36:11 +0100

    Utilize aiorun for clean shutdowns

--- knoepfe/__main__.py
+++ knoepfe/__main__.py
@@ -14,12 +14,11 @@ Options:
   --mock-device   Don't connect to a real device. Mainly useful for debugging.
 """
 
-import asyncio
-import sys
 from asyncio import sleep
 from pathlib import Path
 from textwrap import indent
 
+from aiorun import run
 from docopt import docopt
 from StreamDeck.DeviceManager import DeviceManager
 from StreamDeck.Devices import StreamDeck
@@ -28,53 +27,58 @@ from StreamDeck.Transport.Transport import TransportError
 from knoepfe import __version__, log
 from knoepfe.config import process_config
 from knoepfe.deckmanager import DeckManager
-from knoepfe.log import debug, error, info
+from knoepfe.log import debug, info
 from knoepfe.mockdeck import MockDeck
 
 
-async def connect_device() -> StreamDeck:
-    info("Searching for devices")
-    device = None
+class Knoepfe:
+    def __init__(self) -> None:
+        self.device = None
 
-    while True:
-        devices = DeviceManager().enumerate()
-        if len(devices):
-            device = devices[0]
-            break
-        await sleep(1.0)
-
-    device.open()
-    device.reset()
-
-    info(
-        f"Connected to {device.deck_type()} {device.get_serial_number()} "
-        f"(Firmware {device.get_firmware_version()}, {device.key_layout()[0]}x{device.key_layout()[1]} keys)"
-    )
-
-    return device
-
-
-async def run(config_path: Path | None, mock_device: bool = False) -> None:
-    try:
-        debug("Processing config")
-        global_config, active_deck, decks = process_config(config_path)
-    except Exception as e:
-        raise RuntimeError(f'Failed to parse configuration:\n{indent(str(e), "  ")}')
-
-    while True:
-        if not mock_device:
-            device = await connect_device()
-        else:
-            device = MockDeck()
+    async def run(self, config_path: Path | None, mock_device: bool = False) -> None:
         try:
-            deck_manager = DeckManager(active_deck, decks, global_config, device)
-            await deck_manager.run()
-        except TransportError:
-            debug("Transport error, trying to reconnect")
-            continue
-        finally:
+            debug("Processing config")
+            global_config, active_deck, decks = process_config(config_path)
+        except Exception as e:
+            raise RuntimeError(
+                f'Failed to parse configuration:\n{indent(str(e), "  ")}'
+            )
+
+        while True:
+            device = await self.connect_device() if not mock_device else MockDeck()
+
+            try:
+                deck_manager = DeckManager(active_deck, decks, global_config, device)
+                await deck_manager.run()
+            except TransportError:
+                debug("Transport error, trying to reconnect")
+                continue
+
+    async def connect_device(self) -> StreamDeck:
+        info("Searching for devices")
+        device = None
+
+        while True:
+            devices = DeviceManager().enumerate()
+            if len(devices):
+                device = devices[0]
+                break
+            await sleep(1.0)
+
+        device.open()
+        device.reset()
+
+        info(
+            f"Connected to {device.deck_type()} {device.get_serial_number()} "
+            f"(Firmware {device.get_firmware_version()}, {device.key_layout()[0]}x{device.key_layout()[1]} keys)"
+        )
+
+        return device
+
+    def shutdown(self) -> None:
+        if self.device:
             debug("Closing device")
-            device.close()
+            self.device.close()
 
 
 def main() -> None:
@@ -84,10 +88,10 @@ def main() -> None:
     mock_device = arguments["--mock-device"]
     log.verbose = arguments["--verbose"]
 
-    try:
-        asyncio.run(run(config_path, mock_device))
-    except Exception as e:
-        if log.verbose:
-            raise
-        error(f"{e}")
-        sys.exit(2)
+    knoepfe = Knoepfe()
+
+    run(
+        knoepfe.run(config_path, mock_device),
+        stop_on_unhandled_errors=True,
+        shutdown_callback=lambda _: knoepfe.shutdown(),
+    )
--- tests/test_main.py
+++ tests/test_main.py
@@ -3,50 +3,42 @@ from unittest.mock import AsyncMock, Mock, patch
 from pytest import raises
 from StreamDeck.Transport.Transport import TransportError
 
-from knoepfe.__main__ import connect_device, main, run
+from knoepfe.__main__ import Knoepfe, main
 
 
 def test_main_success() -> None:
-    with patch("knoepfe.__main__.run") as run, patch("sys.argv", ["knoepfe"]):
+    with patch("knoepfe.__main__.run"), patch("knoepfe.__main__.Knoepfe") as knoepfe:
         main()
-    assert run.called
-
-    with patch("knoepfe.__main__.run", side_effect=Exception("Error!")) as run, patch(
-        "sys.argv", ["knoepfe"]
-    ):
-        with raises(SystemExit):
-            main()
-    assert run.called
-
-    with patch("knoepfe.__main__.run", side_effect=Exception("Error!")) as run, patch(
-        "sys.argv", ["knoepfe", "--verbose"]
-    ):
-        with raises(Exception):
-            main()
-    assert run.called
+    assert knoepfe.return_value.run.called
 
 
 async def test_run() -> None:
+    knoepfe = Knoepfe()
+
     with patch("knoepfe.__main__.process_config", side_effect=Exception("Error")):
         with raises(Exception):
-            await run(None)
+            await knoepfe.run(None)
 
-    with patch.multiple(
+    with patch.object(
+        knoepfe, "connect_device", AsyncMock(return_value=Mock())
+    ), patch.multiple(
         "knoepfe.__main__",
         process_config=Mock(return_value=({}, Mock(), [Mock()])),
-        connect_device=AsyncMock(return_value=Mock()),
         DeckManager=Mock(
             return_value=Mock(run=Mock(side_effect=[TransportError(), SystemExit()]))
         ),
     ):
         with raises(SystemExit):
-            await run(None)
+            await knoepfe.run(None)
 
 
 async def test_connect_device() -> None:
+    knoepfe = Knoepfe()
+
     with patch(
         "knoepfe.__main__.DeviceManager.enumerate",
         side_effect=([], [Mock(key_layout=Mock(return_value=(2, 2)))]),
     ) as device_manager_enumerate, patch("knoepfe.__main__.sleep", AsyncMock()):
-        await connect_device()
+        await knoepfe.connect_device()
+
     assert device_manager_enumerate.called
